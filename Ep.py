17359368083e46b74cc67b3ed4986b417836e88b
FernandoBichuette <fernandoba2@al.insper.edu.br>
2020-03-23 16:33:28 -0300

    parte 1

---
+++ Ep.py
@@ -0,0 +1,18 @@
+running = True
+mao=100
+
+while running:
+    jogador = input("O que voce deseja fazer? apostar/sair: ")
+
+    if jogador == 'sair':
+        print(mao)
+        running=False
+
+    else:
+        
+
+
+
+
+
+
