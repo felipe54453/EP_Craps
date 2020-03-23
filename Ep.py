75291a18a8117de9e885a20e6ef537f97814ca86
FernandoBichuette <fernandoba2@al.insper.edu.br>
2020-03-23 17:21:52 -0300

    atu

--- Ep.py
+++ Ep.py
@@ -1,15 +1,36 @@
+import random 
+
 running = True
 mao=100
-
+aposta=10
 while running:
     jogador = input("O que voce deseja fazer? apostar/sair: ")
-
+    
     if jogador == 'sair':
         print(mao)
         running=False
 
-    else:
+    if jogador == 'apostar': 
+        while True:
+            dado1=random.randint(1,6)
+            dado2=random.randint(1,6)
+
+            if dado1+dado2 == 7 or dado1+dado2==11:
+                mao=mao+ aposta +10
+                running = False 
+
+            if dado1+dado2 == 2 or dado1+dado2==3 or dado1+dado2==12:
+                mao=mao-aposta
+                running = False 
+
+            else:
+                running=False
+
         
+
+
+
+
 
 
 
---
+++ teste.py
@@ -0,0 +1,14 @@
+import random
+
+running = True
+mao=100
+
+while running:
+    jogador = input("O que voce deseja fazer? apostar/sair: ")
+
+    if jogador == 'sair':
+        print(mao)
+        running=False
+
+    else:   
+        print('oi')
