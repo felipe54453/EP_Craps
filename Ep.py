77b3ce27becccb84ed316b878188fcf28cf65385
FernandoBichuette <fernandoba2@al.insper.edu.br>
2020-03-25 16:18:13 -0300

    1 parte

--- Ep.py
+++ Ep.py
@@ -3,29 +3,40 @@ import random
 running = True
 mao=100
 aposta=10
+
+def dados():
+    dado_1=random.randint(1,6)
+    dado_2=random.randint(1,6)
+
+    return dado_1,dado_2
+
 while running:
     jogador = input("O que voce deseja fazer? apostar/sair: ")
-    
+      
+    if jogador == 'apostar':
+        #aposta = float(input('Qual o valor da aposta?: '))
+        game=True
+        while game:
+            dado1,dado2 = dados()
+            soma=dado1+dado2
+            print('Dados tirados: ',dado1,dado2,'|','Soma: ',soma)
+           
+            if soma == 7 or soma==11:
+                mao=mao+ aposta +10
+                game = False 
+
+            if soma == 2 or soma==3 or soma==12: #craps
+                mao=mao-aposta
+                game = False 
+
+            else: #Point
+                game=False
+
     if jogador == 'sair':
         print(mao)
         running=False
 
-    if jogador == 'apostar': 
-        while True:
-            dado1=random.randint(1,6)
-            dado2=random.randint(1,6)
-
-            if dado1+dado2 == 7 or dado1+dado2==11:
-                mao=mao+ aposta +10
-                running = False 
-
-            if dado1+dado2 == 2 or dado1+dado2==3 or dado1+dado2==12:
-                mao=mao-aposta
-                running = False 
-
-            else:
-                running=False
-
+    
         
 
 
--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import random
-
+'''
 running = True
 mao=100
 
@@ -12,3 +12,20 @@ while running:
 
     else:   
         print('oi')
+'''
+'''
+gf=float(input('valor'))
+
+print(gf)
+'''
+
+def dados():
+    dado1=random.randint(1,6)
+    dado2=random.randint(1,6)
+
+    return dado1,dado2
+
+print(sum(dados()))
+
+dado1,dado2=dados()
+print(dado1,dado2,dado1+dado2)
